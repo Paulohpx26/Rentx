e066bb16dc371e5d4c7f7a6680620de59bf49e82
Paulo Andrade <paulo.gash@hotmai.com>
2022-07-11 01:26:51 -0300

    feature: upload images on aws storage

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: '55daecc6ed547eca265d9fe0f3d16030',
-  expires_in_token: '15m',
+  expires_in_token: '200d',
   secret_refresh_token: '86bbf5d3cb5b6902e7619a2d90954f45',
   expires_in_refresh_token: '30d',
   expires_refresh_token_days: 30,
--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import { ICarImagesRepository } from '@modules/cars/repositories/ICarImagesRepository';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
 
 interface IRequest {
   car_id: string;
@@ -12,13 +13,21 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarImagesRepository')
     private carImagesRepository: ICarImagesRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     await Promise.all(
-      images_name.map(image_name =>
-        this.carImagesRepository.create(car_id, image_name),
-      ),
+      images_name.reduce((previousValue, currentValue) => {
+        previousValue.push(
+          this.storageProvider.save(currentValue, 'cars'),
+          this.carImagesRepository.create(car_id, currentValue),
+        );
+
+        return previousValue;
+      }, []),
     );
   }
 }
