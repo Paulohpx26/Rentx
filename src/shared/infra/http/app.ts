554d8d318d81af5676eacf9889843df03588c3ab
Paulo <paulo.gash@hotmail.com>
2022-10-27 01:07:07 -0300

    feature: added sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import upload from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { AppError } from '@shared/errors/AppError';
 import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import createConnection from '@shared/infra/typeorm';
@@ -19,6 +21,20 @@ const app = express();
 
 app.use(rateLimiter);
 
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+}
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -28,6 +44,10 @@ app.use('/cars', express.static(`${upload.tmpFolder}/avatar`));
 
 app.use(cors());
 app.use(router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(Sentry.Handlers.errorHandler());
+}
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
